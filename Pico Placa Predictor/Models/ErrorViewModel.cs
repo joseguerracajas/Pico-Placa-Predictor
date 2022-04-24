660cc64f421428e06575fd5532bd0e55e68ced8f
joseguerracajas <josefo_gue17@hotmail.com>
2022-04-24 11:19:02 -0500

    Initial project setup
    Initial project setup using ASP.NET Core 6

---
+++ Pico Placa Predictor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pico_Placa_Predictor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
